f5130dd8f18af7b066e437686940442c01824ab8
Jessica González Bautista <jessicabautista1698@gmail.com>
2021-06-15 21:56:31 -0500

    tutoria eliminado email

--- codigo/src/main/java/mx/uady/sicei/service/TutoriaService.java
+++ codigo/src/main/java/mx/uady/sicei/service/TutoriaService.java
@@ -98,24 +98,15 @@ public class TutoriaService{
     }
 
     public void borrarTutoria(Integer idalumno, Integer idprofesor){
-        // TutoriaLlave tutoriaLlave = new TutoriaLlave(); 
-
+        
         Tutoria tutoria = new Tutoria(); 
         
-        // Alumno alumno= alumnoRepository.findById(idalumno)
-        //     .orElseThrow(() -> new NotFoundException("El alumno no se encuentra."));
-
-        // Profesor profe = profesorRepository.findById(idprofesor)
-        //     .orElseThrow(() -> new NotFoundException("El profesor no se encuentra."));
-
-        // tutoriaLlave.setIdAlumno(idalumno);
-        // tutoriaLlave.setIdProfesor(idprofesor);
         tutoria=this.getTutoria(idalumno, idprofesor);
         tutoriaRepository.deleteById(tutoria.getId());
         
-        System.out.println("Querid@ "+tutoria.getAlumno().getNombre()+", la tutoría asignada con el profesor "+
-        tutoria.getProfesor().getNombre()+" con una duración de " + tutoria.getHoras().toString() +" ha sido eliminada. :("+
-        tutoria.getAlumno().getUsuario().getEmail()+
+        authService.enviarCorreo("Querid@ "+tutoria.getAlumno().getNombre()+", la tutoría asignada con el profesor "+
+        tutoria.getProfesor().getNombre()+" con una duración de " + tutoria.getHoras().toString() +" ha sido eliminada. :(",
+        tutoria.getAlumno().getUsuario().getEmail(),
          "Tutoría eliminada");
     }
 }
